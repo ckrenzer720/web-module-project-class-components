71be144fded5fa9e764f19605281c97d8dd3d64f
Conner Krenzer <krenzer84@gmail.com>
2024-10-08 14:25:47 -0400

    onChanged

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -3,13 +3,25 @@ import React from 'react'
 export default class Form extends React.Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      name: '',
+    }
+  }
+  onSubmit = evt => {
+    
+  }
+  onChange = evt => {
+    this.setState({
+      name: evt.target.value
+    })
   }
   render() {
+    const { addTodo } = this.props
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <input type="text" value={this.state.name} onChange={this.onChange} />
+        <input type="submit" value="submit :(" />
+      </form>
     )
   }
 }
