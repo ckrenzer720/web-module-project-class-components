981f41fab9f9630793383cf75c1942006a183800
Conner Krenzer <krenzer84@gmail.com>
2024-10-08 16:54:49 -0400

    fin?

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,48 +1,54 @@
-import React, { useState } from 'react'
-import Form from './Form'
-import TodoList from './TodoList';
+import React, { useState } from "react";
+import Form from "./Form";
+import TodoList from "./TodoList";
 
-let id = 0
-let getId = () => id++
+let id = 0;
+let getId = () => id++;
 
 const todos = [
-  {name: "Walk the dog", id: getId(), completed: false }, 
-  {name: "Learn React", id: getId(), completed: false}, 
-  {name: "Have fun", id: getId(), completed: false}
-]
+  { name: "Walk the dog", id: getId(), completed: false },
+  { name: "Learn React", id: getId(), completed: false },
+  { name: "Have fun", id: getId(), completed: false },
+];
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: todos
-    }
+      todos: todos,
+    };
   }
 
-  toggleComplete = id => {
-    this.setState({todos: this.state.todos.map(todo => {
-      if (id == todo.id) {
-        return {...todo, completed: !todo.completed}
-      } else {
-        return todo
-      }
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (id == todo.id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      }),
+    });
+  };
+  addTodo = (name) => {
+    this.setState({ ...this.state, 
+      todos: this.state.todos.concat({ id: getId(), completed: false, name: name})});
+  };
+  clearCompleted = () => {
+    this.setState({...this.state, todos: this.state.todos.filter(todo => {
+      if (!todo.completed) return todo;
     })})
-  }
-  addTodo = (e, todo ) => {
-    e.preventDefault()
-    const newTodo = {
-      name: todo,
-      id: getId(),
-      completed: false
-    }
-    this.setState({...this.state, todos: [...this.state.todos, newTodo]})
   }
   render() {
     return (
       <div>
-        <TodoList todos={this.state.todos} toggleComplete={this.toggleComplete}/>
+        <TodoList
+          todos={this.state.todos}
+          toggleComplete={this.toggleComplete}
+        />
         <Form addTodo={this.addTodo} />
+        <input type="button" value="Hide Completed" onClick={this.clearCompleted}/>
       </div>
-    )
+    );
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,25 +2,31 @@ import React from 'react'
 
 export default class Form extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       name: '',
     }
   }
   onSubmit = evt => {
-    
+    evt.preventDefault()
+    this.props.addTodo(this.state.name)
+    this.setState({
+      ...this.state,
+      name: ''
+    })
   }
   onChange = evt => {
     this.setState({
+      ...this.state,
       name: evt.target.value
     })
   }
+  
   render() {
-    const { addTodo } = this.props
     return (
       <form onSubmit={this.onSubmit}>
-        <input type="text" value={this.state.name} onChange={this.onChange} />
-        <input type="submit" value="submit :(" />
+        <input type="text" value={this.state.name} onChange={this.onChange} placeholder='Type todo'/>
+        <input type="submit"/>
       </form>
     )
   }
